60ec124b535117dc7bb82ff5435c0e8785212c32
Alen Lo <yunghua.lo@gmail.com>
2015-10-05 17:55:21 +0800

    讀取設定檔以及交易月份代碼表

---
+++ TradeUtility/ConfigFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TradeUtility
+{
+    public class ConfigFile : TradeFile
+    {
+
+        public ConfigFile(String fileName)
+            : base(fileName)
+        {
+
+        }
+
+        public string readConfig(string headerText)
+        {
+
+            string config = string.Empty;
+            string header = string.Empty;
+            string context = string.Empty;
+            string[] rows;
+
+            while (this.hasNext())
+            {
+                try
+                {
+                    config = this.getLine();
+
+                    if (config.StartsWith("##"))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        rows = config.Split('=');
+                        header = rows[0].Trim();
+                        context = rows[1].Trim();
+
+                        if (header.Equals(headerText))
+                        {
+                            return context;
+                        }
+
+                    }
+
+                }
+                catch (Exception )
+                {
+                    //Console.WriteLine(e.Message);
+                    //Console.WriteLine(e.Source);
+                    //Console.WriteLine(e.StackTrace);
+                    continue;
+                }
+                
+            }
+            return null;
+        }//end of readConfig
+
+
+    }
+}
---
+++ TradeUtility/TradeUtility.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TradeUtility
+{
+    public class TradeUtility
+    {
+        private static TradeUtility utility = null;
+
+        const string monthFilePath = "C:/Trader/TradeMonthCode.txt";
+
+        static ConfigFile monthFile;//月份代碼設定檔
+
+        private TradeUtility()
+        {
+            try
+            {
+                monthFile = new ConfigFile(monthFilePath);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            if (monthFile.isExist())
+            {
+                monthFile.prepareReader();
+            }
+            else
+            {
+                throw new Exception(monthFilePath + "不存在！");
+            }
+        }
+
+        ~TradeUtility()
+        {
+            monthFile.close();
+        }
+
+        public static TradeUtility getInstance()
+        {
+            try
+            {
+                if (utility == null)
+                {
+                    utility = new TradeUtility();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return utility;
+        }
+
+        private string dealTradMonth(DateTime now)//用日期計算出交易月份是哪一個月
+        {
+            // 本月第一天
+            DateTime firstDay = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1, 0, 0, 0);
+
+            // 找到第一個星期三
+            int wk;//星期幾
+
+            wk = int.Parse(firstDay.DayOfWeek.ToString("d"));
+
+            while (wk != 3)
+            {
+                firstDay = firstDay.AddDays(1);
+                wk = (wk + 1) % 7;
+            }
+
+            DateTime thridWeekWendesday = firstDay.AddDays(14);
+
+            if (now.Day > thridWeekWendesday.Day)
+            {
+                return Convert.ToString(now.Month + 1);
+            }
+            else
+            {
+                return Convert.ToString(now.Month);
+            }
+
+
+        }//end dealTradMonth
+
+        private string dealMonthCode(string tradeMonth)//元大期貨交易月份代碼
+        {
+            return monthFile.readConfig(tradeMonth);
+        }
+
+        public string dealTradeCode(string code)//自動計算當天的商品代碼
+        {
+            try
+            {
+                DateTime now = System.DateTime.Now;                
+
+                string tradeMonth = "";
+
+                tradeMonth = dealTradMonth(now);//交易月份
+
+                string monthCode = dealMonthCode(tradeMonth);
+                code += monthCode;
+
+                string yearCode = Convert.ToString(now.Year % 10);
+
+                code += yearCode;
+
+                return code;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source);
+                Console.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+
+    }
+}
